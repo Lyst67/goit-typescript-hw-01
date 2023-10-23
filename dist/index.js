930759e4ac7154b681ba685713b627b44b28e208
Lyst67 <p_listopad@ukr.net>
2023-10-23 22:17:39 +0300

    gitignore

--- dist/index.js
+++ dist/index.js
@@ -4,4 +4,11 @@ const button = document.querySelector('button');
 if (button && input) {
     button.addEventListener('click', () => { concatenation(input.value, 'Hello!'); });
 }
+const nunbers = [1, 2, 3,];
+nunbers.push(4);
+// type User = { name: string };
+// function gread(user: User): void {
+//     console.log(`Hello! ${user.name}`)
+// }
+// gread({ name: 'Pol' })
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,12 @@ const button = document.querySelector('button');
 if (button && input) {
     button.addEventListener('click',  () => { concatenation(input.value, 'Hello!'); })
 }
+
+const nunbers: number[] = [1, 2, 3,]
+nunbers.push(4);
+
+// type User = { name: string };
+// function gread(user: User): void {
+//     console.log(`Hello! ${user.name}`)
+// }
+// gread({ name: 'Pol' })
